6a7113a7607442764673134a95fa4edc1651213d
Avinash <ak7797405@gmail.com>
2024-07-25 13:22:26 +0530

    added JWT to all

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,8 +5,8 @@
 
  const router = express.Router();
 
- router.get("/",getPosts);
- router.get("/:id",getPost);
+ router.get("/",verifyToken,getPosts);
+ router.get("/:id",verifyToken,getPost);
  router.post("/",verifyToken,addPost);
  router.put("/:id",verifyToken,updatePost);
  router.delete("/:id",verifyToken,deletePost);
